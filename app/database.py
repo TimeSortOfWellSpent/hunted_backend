33036e31b5b67a0348990c5b95259036c3be0ed0
scr1pticle <erpoluden@gmail.com>
2025-05-21 20:15:48 +0200

    added basic models and a test user route

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlmodel import create_engine, SQLModel, Session
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:admin@db/postgres'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
